77bb220aa39f0febda7c68b7a66875cecd9845ac
AP_MejiaMendez <00098225@uca.edu.sv>
2025-04-28 14:59:33 -0600

    Update

--- Ejemplo/Ejercicio.cpp
+++ Ejemplo/Ejercicio.cpp
@@ -3,24 +3,29 @@ using namespace std;
 
 int main()
 {
+    //Declaracion de variable que guardará la opción del usuario
     int opcion = 0;
-    cout << "Que eres? 1 = Malo, 2 = Animal 3 = Persona" << endl;
-    cin >> opcion;
+    cout << "Que eres? 1 = Malo, 2 = Animal 3 = Persona" << endl; //Mensaje que da contexto
+    cin >> opcion; //Se asigna a la variable la opcion del usuario
 
-    if (opcion == 1)
+    if (opcion == 1) 
     {
+        //Si el usuario digitó 1, apaecerá el siguiente mensaje
         cout << "Estas dentro de mi lista de presas";
     }
     else if (opcion == 2)
     {
+        //Si el usuario digitó 2, apaecerá el siguiente mensaje
         cout << "Te voy a proteger";
     }
     else if (opcion == 3)
     {
+        //Si el usuario digitó 3, apaecerá el siguiente mensaje
         cout << "Hola que tal";
     }
     else
     {
+        //Si el usuario no figitó 1, 2 o 3, apaecerá el siguiente mensaje
         cout << "Nada que decirte";
     }
 }
